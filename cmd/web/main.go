2888c326215548aa48ee15be79160cf48fdf1aef
frdnryann <frdnryann@gmail.com>
2026-08-10 17:04:24 +0700

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello!, welcome to my website")
+		fmt.Println("[LOG] : User mengunjungi halaman utama")
+	})
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
